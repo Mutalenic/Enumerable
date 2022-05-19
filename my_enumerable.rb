096ccae107d976cbf5f3fc77487636a82ae9a044
Nicholas Mutale <nicomutale@gmail.com>
2022-05-19 17:24:04 +0200

    Add filter method

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -4,10 +4,16 @@ module MyEnumerable
     each { |item| return true if yield item }
     false
   end  
-end
 
 def any?
     each { |item| return true if yield item }
     false
   end  
+
+def filter
+    filter_list = []
+    each { |item| filter_list.push(item) if yield item }
+    filter_list
+  end  
 end
+
