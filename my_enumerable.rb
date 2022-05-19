65c97d2be1b9cc2c16fa11e31032570cef6a08d6
Nicholas Mutale <nicomutale@gmail.com>
2022-05-19 16:55:25 +0200

    Add any method

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -2,5 +2,12 @@ module MyEnumerable
 
   def all?
     each { |item| return true if yield item }
-  end
+    false
+  end  
+end
+
+def any?
+    each { |item| return true if yield item }
+    false
+  end  
 end
